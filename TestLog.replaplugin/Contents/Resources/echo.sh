8db91fda9c6d7ee6363721d9792e260092ae8997
Roben Kleene <contact@robenkleene.com>
2020-10-20 18:06:18 -0400

    First commit

---
+++ TestLog.replaplugin/Contents/Resources/echo.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+set -e
+
+echo 'Testing log error' >&2
+printf "\n" >&2
+echo 'ERROR ' >&2
+echo 'Testing log message'
+echo 'MESSAGE '
+echo
+printf "\t"
+printf "\n"
+echo 'Done'
+printf "\n"
+
