5feeef88a4a9aae2df5f7e18f3b9d8163a2ef702
Roben Kleene <contact@robenkleene.com>
2022-05-07 04:34:31 -0400

    Redirect standard error to standard out

--- packages/TestServer.replaplugin/Contents/Resources/server.sh
+++ packages/TestServer.replaplugin/Contents/Resources/server.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-ruby -run -e httpd -- -p 0 . | while read -r line; do
+ruby -run -e httpd -- -p 0 . 2>&1 | while read -r line; do
   echo "$line"
   if [[ $line =~ port=([0-9]+)$ ]]; then
     port="${BASH_REMATCH[1]}"
