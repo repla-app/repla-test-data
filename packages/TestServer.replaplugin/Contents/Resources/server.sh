ff6d61bc4b43870cbaae9bc79190ad3e7541aef0
Roben Kleene <contact@robenkleene.com>
2022-05-07 05:03:44 -0400

    Add option for setting title

--- packages/TestServer.replaplugin/Contents/Resources/server.sh
+++ packages/TestServer.replaplugin/Contents/Resources/server.sh
@@ -2,10 +2,36 @@
 
 set -e
 
+title="false"
+while getopts ":p:th" option; do
+  case "$option" in
+    p)
+      file_path="$OPTARG"
+      ;;
+    t)
+      title="true"
+      ;;
+    h)
+      echo "Usage: command [-hf] [-p <file_path>]"
+      exit 0
+      ;;
+    :)
+      echo "Option -OPTARG requires an argument" >&2
+      exit 1
+      ;;
+    \?)
+      echo "Invalid option: -OPTARG" >&2
+      exit 1
+      ;;
+  esac
+done
+
 ruby -run -e httpd -- -p 0 . 2>&1 | while read -r line; do
   echo "$line"
-  if [[ $line =~ port=([0-9]+)$ ]]; then
-    port="${BASH_REMATCH[1]}"
-    echo "$port"
+  if [[ "$title" == "true" ]]; then
+    if [[ $line =~ port=([0-9]+)$ ]]; then
+      port="${BASH_REMATCH[1]}"
+      osascript -e "tell application \"Repla\" to do JavaScript \"document.title = '$port'\" in window 1"
+    fi
   fi
 done
