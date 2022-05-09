d9406905b32ed02cfce810941963e8b81ff4b153
Roben Kleene <contact@robenkleene.com>
2022-05-09 05:05:45 -0400

    Fix window ID

--- packages/TestServer.replaplugin/Contents/Resources/server.sh
+++ packages/TestServer.replaplugin/Contents/Resources/server.sh
@@ -7,7 +7,7 @@ ruby -run -e httpd -- -p 0 . 2>&1 | while read -r line; do
   if [[ -n "${REPLA_WINDOW_ID-}" ]]; then
     if [[ $line =~ port=([0-9]+)$ ]]; then
       port="${BASH_REMATCH[1]}"
-      osascript -e "tell application \"Repla\" to do JavaScript \"document.title = '$port'\" in window $REPLA_WINDOW_ID"
+      osascript -e "tell application \"Repla\" to do JavaScript \"document.title = '$port'\" in window id $REPLA_WINDOW_ID"
     fi
   fi
 done
