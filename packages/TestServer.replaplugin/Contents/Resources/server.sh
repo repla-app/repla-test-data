021f730dc7d16f27a34f1d437fb8e68c55821232
Roben Kleene <contact@robenkleene.com>
2022-05-11 05:20:17 -0400

    Make port available (#3)
    * Add wrapper to parse line
    
    * Work on extracting port
    
    * Redirect standard error to standard out
    
    * Add option for setting title
    
    * Instead check for a window ID
    
    * Fix window ID check
    
    * Fix window ID

--- packages/TestServer.replaplugin/Contents/Resources/server.sh
+++ packages/TestServer.replaplugin/Contents/Resources/server.sh
@@ -2,5 +2,12 @@
 
 set -e
 
-ruby -run -e httpd -- -p 0 .
-
+ruby -run -e httpd -- -p 0 . 2>&1 | while read -r line; do
+  echo "$line"
+  if [[ -n "${REPLA_WINDOW_ID-}" ]]; then
+    if [[ $line =~ port=([0-9]+)$ ]]; then
+      port="${BASH_REMATCH[1]}"
+      osascript -e "tell application \"Repla\" to do JavaScript \"document.title = '$port'\" in window id $REPLA_WINDOW_ID"
+    fi
+  fi
+done
